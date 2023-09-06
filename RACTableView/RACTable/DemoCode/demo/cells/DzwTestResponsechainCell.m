33ad21401fe6157476aa89f0f2d2fed615741eda
Aaronduan <aaronduan@zego.im>
2023-09-06 18:48:48 +0800

    Update DzwTestResponsechainCell.m
    update detail

--- RACTableView/RACTable/DemoCode/demo/cells/DzwTestResponsechainCell.m
+++ RACTableView/RACTable/DemoCode/demo/cells/DzwTestResponsechainCell.m
@@ -19,7 +19,26 @@
 
 - (void)awakeFromNib {
     [super awakeFromNib];
-    // Initialization code
+    self.alphaBtn.titleLabel.font = kFontWithName(kMedFont, 13.f);
+    self.alphaBtn.layer.cornerRadius = 5;
+    self.alphaBtn.backgroundColor = [HexColor(@"#E9483A") colorWithAlphaComponent:0.3];
+    self.alphaBtn.layer.borderColor = HexColor(@"#E9483A").CGColor;
+    self.alphaBtn.layer.borderWidth = 0.5;
+    self.alphaBtn.layer.masksToBounds = YES;
+    
+    self.betaBtn.titleLabel.font = kFontWithName(kMedFont, 13.f);
+    self.betaBtn.layer.cornerRadius = 5;
+    self.betaBtn.backgroundColor = [HexColor(@"#FBBD1C") colorWithAlphaComponent:0.3];
+    self.betaBtn.layer.borderColor = HexColor(@"#FBBD1C").CGColor;
+    self.betaBtn.layer.borderWidth = 0.5;
+    self.betaBtn.layer.masksToBounds = YES;
+    
+    self.gamaBtn.titleLabel.font = kFontWithName(kMedFont, 13.f);
+    self.gamaBtn.layer.cornerRadius = 5;
+    self.gamaBtn.backgroundColor = [HexColor(@"#30A14C") colorWithAlphaComponent:0.3];
+    self.gamaBtn.layer.borderColor = HexColor(@"#30A14C").CGColor;
+    self.gamaBtn.layer.borderWidth = 0.5;
+    self.gamaBtn.layer.masksToBounds = YES;
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
@@ -34,9 +53,15 @@
 }
 
 - (IBAction)betaAction:(UIButton *)sender {
+    [self routerEventWithName:DzwCellEventName(_cmd) userInfo:@{DzwCellEventName(_cmd):sender,
+                                                                DzwCellIndexPath:self.cellModel.indexPath
+                                                              }];
 }
 
 - (IBAction)gamaAction:(UIButton *)sender {
+    [self routerEventWithName:DzwCellEventName(_cmd) userInfo:@{DzwCellEventName(_cmd):sender,
+                                                                DzwCellIndexPath:self.cellModel.indexPath
+                                                              }];
 }
 
 @end
